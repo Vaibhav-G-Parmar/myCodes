1a2a811739215ba2ba7ba1ef4e490470ac230586
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-01-05 03:03:02 -0500

    Updated logic folder

---
+++ Logic and Decisions/vowelOrNo/isCharVowel.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   char ch;
+   bool isOver = true;
+   cout << "*** Check out if an entered character is Vowel or Consonant! ***" << endl << endl;
+
+   do
+   {
+      cout << "Please enter a character" << endl << "Enter 0 to exit" << endl << "> ";
+      cin >> ch;
+      cout << endl;
+
+      if (ch == '0')
+      {
+         isOver = false;
+      }
+      else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+      {
+         cout << ch << " is an Vowel character!" << endl << endl;
+      }
+      else
+      {
+         cout << ch << " is an Consonant character!" << endl << endl;
+      }
+   } while (isOver);
+   cout << "Thank you hope to see you soon!" << endl;
+   return 0;
+}
