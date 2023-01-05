705821f42467e7b80e83fa096424e8c3fed25572
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-01-05 02:52:17 -0500

    added oddOrEven.cpp

---
+++ loops_logic/oddOrEven/oddOrEven.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num;
+   bool isOver = true;
+   cout << "*** Check out if an entered number is Odd or Even! ***" << endl << endl;
+
+   do
+   {
+      cout << "Please enter a number" << endl << "Enter 0 to exit" << endl << "> ";
+      cin >> num;
+      cout << endl;
+
+      if(num == 0)
+      {
+         isOver = false;
+      }
+      else if (num % 2 == 0)
+      {
+         cout << num << " is an even number!" << endl << endl;
+      }
+      else
+      {
+         cout << num << " is an odd number!" << endl << endl;
+      }
+   } while (isOver);
+   cout << "Thank you hope to see you soon!" << endl;
+   return 0;
+}
