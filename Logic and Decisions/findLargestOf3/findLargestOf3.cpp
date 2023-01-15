2391c67c6b9aa4750b4daf377233de71abf51269
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-01-14 22:32:09 -0500

    updated code

--- Logic and Decisions/findLargestOf3/findLargestOf3.cpp
+++ Logic and Decisions/findLargestOf3/findLargestOf3.cpp
@@ -15,24 +15,17 @@ int main()
 
 	for (int i = 0; i < 3; i++)
 	{
-		cout << "Enter integer " << i+1 << " > ";
+		cout << "Enter integer " << i + 1 << " > ";
 		cin >> num[i];
 	}
 	cout << endl;
 
-	if (num[0] > num[1])
-	{
-		if (num[0] > num[2])
-			cout << "The largest number out of the three is : " << num[0] << endl;
-	}
-	if (num[1] > num[0])
-	{
-		if (num[1] > num[2])
-			cout << "The largest number out of the three is : " << num[1] << endl;
-	}
-	if (num[2] > num[1])
-	{
-		if (num[2] > num[0])
-			cout << "The largest number out of the three is : " << num[2] << endl;
-	}
+	if (num[0] > num[1] && num[0] > num[2])
+		cout << "The largest number out of the three is : " << num[0] << endl;
+
+	else if (num[1] > num[0] && num[1] > num[2])
+		cout << "The largest number out of the three is : " << num[1] << endl;
+
+	else
+		cout << "The largest number out of the three is : " << num[2] << endl;
 }
